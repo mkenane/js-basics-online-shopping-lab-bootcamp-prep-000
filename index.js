936fe020b1244629dda45da7036adf42b6746b51
Mor Kenane <morkenan@hotmail.com>
2017-12-14 00:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -81,8 +81,8 @@ console.log(`Your total cost is $${runTotal}, which will be charged to the card 
 
 
 function placeOrder(creditCard) {
-var x = creditCard 
-if (x == "") { 
+
+if (creditCard === "") { 
   console.log("Sorry, we don't have a credit card on file for you..")}
 
 else {
